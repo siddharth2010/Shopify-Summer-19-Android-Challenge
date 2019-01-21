6e866c812918ebfd45c9ab2792de5255f0398d2b
siddharth2010 <ssiddharth2010@gmail.com>
2019-01-21 16:02:44 +0530

    Add Navigation Button to app Drawer

--- app/src/main/java/com/example/shopifysiddharth/CollectionActivity.java
+++ app/src/main/java/com/example/shopifysiddharth/CollectionActivity.java
@@ -1,11 +1,16 @@
 package com.example.shopifysiddharth;
 
 import android.os.AsyncTask;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.example.shopifysiddharth.adapter.CollectionAdapter;
 
@@ -26,6 +31,7 @@ import java.util.List;
 
 public class CollectionActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawerLayout;
     public String test;
     private RecyclerView recyclerView = null;
     public CollectionAdapter mCollectionAdapter;
@@ -38,12 +44,29 @@ public class CollectionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collection);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_baseline_view_headline_24px);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
         recyclerView = (RecyclerView) findViewById(R.id.rv_collection);
 
         TsunamiAsyncTask task = new TsunamiAsyncTask();
         task.execute();
     }
 
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     private class TsunamiAsyncTask extends AsyncTask<URL, Void, String> {
 
